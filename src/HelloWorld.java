6e952a81ce3935341068f5313ae96ec2ae96d8cf
uradnah <mbhandaru@gmail.com>
2018-09-04 11:02:59 +0700

    Hello World commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
-	public static viod main (String [] args) {
+	public static void main (String [] args) {
 		System.out.println("Hello World again");
 		System.out.println("Hai!");
 	}
